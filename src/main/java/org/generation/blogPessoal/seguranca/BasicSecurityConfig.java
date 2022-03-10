7bb9e8c097255fceb961a9b1b149e2b5eccaf939
Fabiola <fabiolasou.za@live.com>
2022-03-10 14:12:53 -0300

    @fabiolasoufer [Task: Corrigindo camadas]

--- src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
+++ src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
@@ -20,6 +20,10 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService);
+		auth.inMemoryAuthentication()
+		.withUser("fabi")
+		.password(passwordEncoder().encode("fabi"))
+		.authorities("ROLE_USER");
 	}
 	
 	@Bean
