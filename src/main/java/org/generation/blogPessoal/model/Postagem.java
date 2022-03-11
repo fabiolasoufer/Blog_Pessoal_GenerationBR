9aa64a36542b1c878ed934ebf51f8b2d556e0fad
Fabiola <fabiolasou.za@live.com>
2022-03-11 10:33:08 -0300

    @fabiolasoufer [Task: Excluindo anotações de postagens]

--- src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -15,18 +15,7 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-/**
- * English:
- * Class responsible for abstracting objects and tables in our database. 
- * Attributes used for Post: id, title, text, date. This class also has a relationship with the Theme model.
- * 
- * Português:
- * Classe responsável pela abstração de objetos e tabelas em nosso banco de dados. 
- * Atributos utilizados para Postagem: id, título, texto, data. 
- * Essa classe também possui relacionamento com a model Tema.
- * 
- * @author Fabíola
- */
+
 @Entity
 @Table(name = "postagem")
 public class Postagem {
@@ -46,17 +35,7 @@ public class Postagem {
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date data = new java.sql.Date(System.currentTimeMillis());
 	
-	/**
-	 * English:
-	 * Relationship with the Theme model, too many posts for one theme. 
-	 * And JsonIgnoreProperties annotation to inhibit recursion.
-	 * 
-	 * Português:
-	 * Relacionamento com a model Tema, muitas postagens para um tema. 
-	 * E anotação JsonIgnoreProperties para inibir recursividade. 
-	 * 
-	 * @author Fabíola
-	 */
+	
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
--- src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
+++ src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -6,15 +6,7 @@ import org.generation.blogPessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * English:
- * Interface responsible for direct communication with the database.
- * 
- * Português:
- * Interface responsável pela comunicação direta com o banco de dados.
- * 
- * @author Fabíola
- */
+
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
